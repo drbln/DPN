10078c0a3d51b19ecd9bc36f1fe9b88feefb8ee0
Rudnev Oleg <r_oleg94@mail.ru>
2022-07-18 17:22:12 +0300

    two committwo commit

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "wait" {
   provisioner "local-exec" {
-    command = "sleep 100"
+    command = "sleep 40"
   }
 
   depends_on = [
@@ -8,9 +8,19 @@ resource "null_resource" "wait" {
   ]
 }
 
-resource "null_resource" "cluster" {
+resource "null_resource" "nginx" {
   provisioner "local-exec" {
     command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory/prod.yml ../ansible/nginx.yml"
+  }
+
+  depends_on = [
+    null_resource.wait
+  ]
+}
+
+resource "null_resource" "db" {
+  provisioner "local-exec" {
+    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory/prod.yml ../ansible/db.yml"
   }
 
   depends_on = [
