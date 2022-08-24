9ff940b03f22533c71867863fd09820689820c6a
Rudnev Oleg <r_oleg94@mail.ru>
2022-08-24 15:24:59 +0300

    edited inventory.tf

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -6,9 +6,9 @@ resource "local_file" "inventory" {
     yandex_compute_instance.vm-1 ansible_ssh_host=${yandex_compute_instance.vm-1.network_interface.0.nat_ip_address} ansible_ssh_port=22
     
     [db1]
-    yandex_compute_instance.vm-2 ansible_ssh_host=${yandex_compute_instance.vm-1.network_interface.0.nat_ip_address} ansible_ssh_port=22
+    yandex_compute_instance.vm-2 ansible_ssh_host=192.168.10.11 ansible_ssh_port=22
     [db1:vars]
-    ansible_ssh_common_args='-o ProxyCommand="ssh -W %h:%p -q ubuntu@192.168.10.11"'
+    ansible_ssh_extra_args =-o ControlPersist=7200s -o ServerAliveInterval=60 -o StrictHostKeyChecking=no -J ubuntu@${yandex_compute_instance.vm-1.network_interface.0.nat_ip_address}
     DOC
   filename = "../ansible/inventory/prod.yml"
 
